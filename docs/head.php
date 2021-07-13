dea6f13435d613bd7270209d833b8eeac7a769a9
tldkwnstn <hzeong66@gmail.com>
2021-07-14 01:43:01 +0900

    타이틀 변경, background색상 변경

--- docs/head.php
+++ docs/head.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>디자이너 장희성</title>
+  <title>디자이너 김현정</title>
 
   <!-- 제이쿼리 불러오기 -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
